2529623c3052e2afb463a08cf787baccb02236c7
Danny Weitekamp <dannyweitekamp@gmail.com>
2019-01-15 03:04:56 -0500

    train.py subprocesses are now linked to it

--- train.py
+++ train.py
@@ -1,14 +1,18 @@
 # Used to start training an Apprentice Learner agent interactively or via example/model tracing. 
+# 
 
 import argparse,sys,os, atexit
 import subprocess, threading
 from subprocess import check_output
 import socket, errno
-import signal
+import signal, time
 from datetime import datetime
 
 al_process = None
 ctat_process = None
+al_thread = None
+ctat_thread = None
+calling_dir = None
 CONFIG_DEFAULT = "net.conf"
 
 
@@ -33,17 +37,23 @@ def read_conf(ns, path):
 
 
 def force_kill_port(port):
+    print("Attempting force kill...")
+
     if("linux" in sys.platform):
         try:
-            pid = check_output(["lsof", "-Pi", ":" + str(port), "-sTCP:LISTEN", "-t"])
-            print("Found process: %s bound to %s") % (pid, port)
-        except:
-            return;
+            tokill =  [int(x) for x in check_output(["lsof", "-Pi", ":" + str(port), "-sTCP:LISTEN", "-t"]).splitlines()]
+            print("Found processes: %s bound to %s" % (tokill, port))
+        except Exception as e:
+            #At this point nothing popped up so we're good
+            return True;
     else:
         raise Warning("Force kill (-f/--force) not implemented for operating system %r" % sys.platform)
-        return;
-    print("Killing process: %s" % pid)
-    os.kill(pid, signal.SIGTERM)
+        return False;
+    print("Killing processes: %s" % tokill)
+    for pid in tokill:
+        kill_group(pid)
+    return True
+    # os.kill(pid, signal.SIGTERM)
 
 
 def check_port(host, port, force=False):
@@ -54,9 +64,16 @@ def check_port(host, port, force=False):
     except socket.error as e:
         if(force):
             
-            force_kill_port(port)
-            return check_port(host,port,False)
+            return force_kill_port(port)
+            # time.sleep(.1)
+            # return check_port(host,port,False)
+        else:
+            print(e)
         return False
+    try:
+        s.shutdown(socket.SHUT_RDWR)
+    except:
+        pass
     s.close()
     return True
 
@@ -65,15 +82,46 @@ def port_error(nm, port):
     sys.exit()
 
 def waitAndExit(proc, onExit):
+
     proc.wait()
     onExit()
 
+def kill_group(p):
+    if(isinstance(p,int)):
+        pgid = os.getpgid(p)
+    else:
+        pgid = os.getpgid(p.pid)
+    print("PGID", pgid)
+    os.killpg(pgid, signal.SIGTERM) 
+
 def kill_all():
-    if(al_process != None): al_process.terminate()
-    if(ctat_process != None): ctat_process.terminate()
+    global al_process,ctat_process
+    print("KILL ALL", al_process.pid,ctat_process)
+    # al_process.stderr = None
+    # ctat_process.stderr = None
+    # al_process.stdout = None
+    # ctat_process.stdout = None
+    # al_process.stderr.close()
+    # ctat_process.stderr.close()
+    # temp_stderr = sys.stderr
+    # sys.stderr = None 
+    # sys.stdout = None 
+    os.killpg(os.getpgid(), signal.SIGTERM) 
+    # sys.stderr = temp_stderr
+    # if(al_process != None): kill_group(al_process)
+    # if(ctat_process != None): kill_group(ctat_process)
+    # if(al_process != None): al_process.terminate()
+    # if(ctat_process != None): ctat_process.terminate()
 
 
-def pasrse_args(argv):
+def apply_wd(path):
+    path = os.path.expandvars(path)
+    if(not os.path.isabs(path)):
+        path = os.path.join(calling_dir,path)
+    return path
+
+
+def parse_args(argv):
     parser = argparse.ArgumentParser(description='Convert ROOT data to numpy arrays stored as HDF5 for Machine Learning use.')
     parser.add_argument('training', type=str, metavar="<training_file>.json",
         help="A JSON file that specifies the sequence of problems to train on.")
@@ -102,6 +150,8 @@ def pasrse_args(argv):
         help="The tab deliminated logging file for the session should go to. By default can be found in /log directory")
     parser.add_argument('--config' ,  default=CONFIG_DEFAULT, dest = "config",      metavar="<config>.conf",
         help="Bash style configuration file used for setting default variables. ")
+    parser.add_argument('-w' , "--working-directory",  default=None, dest = "wd",      metavar="<working-directory>",
+        help="The working directory of the ctat server. By default it is the directory where training.json is located")
 
     try:
         args = parser.parse_args(argv)
@@ -114,9 +164,10 @@ def pasrse_args(argv):
     read_conf(args, args.config)
 
 
-    args.log_dir = os.path.abspath(os.path.expandvars(args.log_dir))
-    args.al_dir = os.path.abspath(os.path.expandvars(args.al_dir))
-    # args.training = os.path.abspath(os.path.expandvars(args.training))
+    args.log_dir = os.path.abspath(apply_wd(args.log_dir))
+    args.al_dir = os.path.abspath(apply_wd(args.al_dir))
+    # os.path.join(calling_dir,args.al_dir)
+    args.training = os.path.os.path.relpath(apply_wd(args.training), start=os.getcwd())
 
 
     assert os.path.isfile(args.training), "No such file %r" % args.training
@@ -130,38 +181,73 @@ def pasrse_args(argv):
         args.output = "%s/%sLog-%s.txt" % (args.log_dir , os.path.basename(args.training).split(".")[0], datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))
 
     
-    args.output = os.path.abspath(os.path.expandvars(args.output))
+    args.output = os.path.abspath(apply_wd(args.output))
 
 
     return args
 
+# RUN = True
+
+# def stop(sig, frame):
+#     print("SIGNAL CAUGHT", sig,al_process,ctat_process)
+#     RUN =False
+#     kill_all()
+
+# signal.signal(signal.SIGTERM, stop )
+# signal.signal(signal.SIGINT, stop)
 
 
 def main(args):
+    global al_process,ctat_process
 
     if(check_port(args.al_host, args.al_port, args.force)):
+        # pass
         al_process =  subprocess.Popen([sys.executable, args.al_dir + "/manage.py", "runserver", str(args.al_host) + ":" + str(args.al_port)])
-        thread = threading.Thread(target=waitAndExit, args=(al_process, kill_all)).start()
+        # al_thread = threading.Thread(target=waitAndExit, args=(al_process, kill_all))
+        # al_thread.start()
     else:
         port_error("AL", args.al_port)
 
     if(check_port(args.ctat_host, args.ctat_port, args.force)):
+        pass
         ctat_process = subprocess.Popen([sys.executable, "src/host_server.py", str(args.ctat_port), args.output])
-        threading.Thread(target=waitAndExit, args=(ctat_process, kill_all)).start()
+        # ctat_thread = threading.Thread(target=waitAndExit, args=(ctat_process, kill_all))
+        # ctat_thread.start()
         
     else:
         port_error("CTAT", args.ctat_port)
 
     ctat_url = "http://localhost:%s/?training=%s&al_url=http://localhost:%s" %(args.ctat_port, args.training,args.al_port)
+    if(args.wd != None): ctat_url += "&wd=" + args.wd
+
     browser_process = subprocess.Popen([args.browser, ctat_url])
-    ctat_process.wait()
+
+    # al_process.wait()
+    print("AL PROCESS")
+    while True:
+        if(al_process.poll() != None or ctat_process.poll() != None):
+            break
+        # try:
+        time.sleep(.1)
+            # ctat_process.wait(.1)
+        # except:
+            # pass
+
+    # ctat_thread.join()
 
     kill_all()
     sys.exit()
 
 if __name__ == "__main__":
+    calling_dir = os.getcwd()
 
-    atexit.register(kill_all);
-    args = pasrse_args(sys.argv[1:])
+    #Always run this script from the directory where it lives
+    abspath = os.path.abspath(__file__)
+    print("ABSPATH", abspath)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    # atexit.register(kill_all);
+    args = parse_args(sys.argv[1:])
     # print(args, type(args))
     main(args)
