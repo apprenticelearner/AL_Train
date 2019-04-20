d633d91c9ad39604d110c378316bd43b0d469885
Danny Weitekamp <dannyweitekamp@gmail.com>
2019-04-20 03:00:55 -0400

    added foci of attention

--- train.py
+++ train.py
@@ -147,6 +147,8 @@ def parse_args(argv):
         help="Force kill processes that hold up ports we want to use. Only implemented for Linux systems.")
     parser.add_argument('-i', '--interactive', action='store_true',default=False, dest = "interactive",
         help="Indicates that AL should trained interactively by the user instead of automatically by example/model tracing.")
+    parser.add_argument('--use-foci', action='store_true',default=False, dest = "use_foci",
+        help="Indicates that AL should trained interactively by the user instead of automatically by example/model tracing.")
 
     parser.add_argument('-a', '--al-port' , default=None, dest = "al_port",     metavar="<AL_port>",
         type=int, help="The port for the apprentice learner server.")
@@ -251,6 +253,7 @@ def main(args):
     ctat_url = "http://localhost:%s/?training=%s&al_url=http://localhost:%s" %(args.ctat_port, args.training,args.al_port)
     if(args.wd != None): ctat_url += "&wd=" + args.wd
     if(args.interactive): ctat_url += "&interactive=true"
+    if(args.use_foci): ctat_url += "&use_foci=true"
 
     
     if(args.browser != None and "selenium" in args.browser):
