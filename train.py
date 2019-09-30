53dca800706225e8b1e9bd3c342a1ded664a38bf
Napol Rachatasumrit <naplr42@gmail.com>
2019-09-30 09:55:48 -0400

    Added no AL option to train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
-# Used to start training an Apprentice Learner agent interactively or via example/model tracing. 
-# 
+# Used to start training an Apprentice Learner agent interactively or via example/model tracing.
+#
 
-import argparse,sys,os, atexit,re 
+import argparse,sys,os, atexit,re
 import subprocess, threading
 from subprocess import check_output
 import socket, errno
@@ -30,7 +30,7 @@ def read_conf(ns, path):
                 val = x[1].strip('\"\'\n \t\f\r');
                 if(val == ""):
                     continue
-                if("port" in key): 
+                if("port" in key):
                     try:
                         val  = int(val)
                     except ValueError as e:
@@ -39,7 +39,7 @@ def read_conf(ns, path):
                             sys.exit()
                 elif("args" in key):
                     val= list(filter(None,re.split(';|,| |\t|\n', val))) # turn into a list of args
-                
+
 
                 setattr(ns, key, val); #Strip quotes and whitespace
     return ns
@@ -72,7 +72,7 @@ def check_port(host, port, force=False):
         s.bind((host, port))
     except socket.error as e:
         if(force):
-            
+
             return force_kill_port(port)
             # time.sleep(.1)
             # return check_port(host,port,False)
@@ -101,7 +101,7 @@ def kill_group(p):
     else:
         pgid = os.getpgid(p.pid)
     print("PGID", pgid)
-    os.killpg(pgid, signal.SIGTERM) 
+    os.killpg(pgid, signal.SIGTERM)
 
 def kill_all():
     global al_process,ctat_process, browser_process
@@ -112,19 +112,19 @@ def kill_all():
     # al_process.stderr.close()
     # ctat_process.stderr.close()
     # temp_stderr = sys.stderr
-    # sys.stderr = None 
-    # sys.stdout = None 
+    # sys.stderr = None
+    # sys.stdout = None
     # if(hasattr(os, "killpg")):
-    #     os.killpg(os.getpgid(), signal.SIGTERM) 
+    #     os.killpg(os.getpgid(), signal.SIGTERM)
     # else:
     if(al_process != None): al_process.terminate()
-    if(ctat_process != None): ctat_process.terminate()    
-    if(browser_process != None): browser_process.terminate()    
+    if(ctat_process != None): ctat_process.terminate()
+    if(browser_process != None): browser_process.terminate()
     if(outer_loop_process != None): outer_loop_process.terminate()
     # sys.stderr = temp_stderr
     # if(al_process != None): kill_group(al_process)
     # if(ctat_process != None): kill_group(ctat_process)
-    
+
 
 
 def apply_wd(path):
@@ -165,6 +165,7 @@ def parse_args(argv):
 
     parser.add_argument('-d', '--al-dir' ,  default=None, dest = "al_dir",      metavar="<AL_dir>",
         help="The directory where the apprentice learner API can be found.")
+    parser.add_argument('--no-al' , action='store_true', help="")
 
     parser.add_argument('-b', '--broswer' , default=None, dest = "browser",     metavar="<browser>",
         help="The browser executable to run CTAT on.")
@@ -182,7 +183,7 @@ def parse_args(argv):
         help="The working directory of the ctat server. By default it is the directory where training.json is located")
     parser.add_argument('-n' , "--nools",  default=None, dest = "nools_dir",      metavar="<nools-out-dir>",
         help="The directory to output the nools production rule code for the agent")
-    
+
     parser.add_argument('--outer-loop', action='store_true', default=False, dest = "outer_loop",
         help="Specifies that an external outer loop server will be used.")
     parser.add_argument('--outer-loop-host' , default='127.0.0.1', dest = "outer_loop_host",     metavar="<outer_loop_host>",
@@ -191,13 +192,13 @@ def parse_args(argv):
         help="Specifies the port to bind to for running the outer loop server.")
     parser.add_argument('--outer-loop-dir', default=None, dest = "outer_loop_dir",      metavar="<outer_loop_dir>",
         help="Specifies the directory of the outer loop repo.")
-    parser.add_argument('--outer-loop-url', default=None, dest = "outer_loop_url",      metavar="<outer_loop_url>", 
-        help="Specifies the URL of a running outer loop server.")   
-    
+    parser.add_argument('--outer-loop-url', default=None, dest = "outer_loop_url",      metavar="<outer_loop_url>",
+        help="Specifies the URL of a running outer loop server.")
+
     try:
         args = parser.parse_args(argv)
         # args.setattr(args, "training", args.training[0]) # dunno why it comes in a list
-        
+
     except Exception:
         parser.print_usage()
         sys.exit()
@@ -205,7 +206,7 @@ def parse_args(argv):
     read_conf(args, args.config)
 
     print(args.browser_args)
-    if(isinstance(args.browser_args, str)): args.browser_args = [args.browser_args] 
+    if(isinstance(args.browser_args, str)): args.browser_args = [args.browser_args]
 
     # args.browser_args = list(filter(None,re.split(';|,| |\t|\n', args.browser_args)))
     # print(args.browser_args)
@@ -218,19 +219,19 @@ def parse_args(argv):
 
     assert os.path.isfile(args.training), "No such file %r" % args.training
     # assert args.al_port != None, "AL_PORT not specified or set in %s" % args.config
-    # assert args.ctat_port != None, "CTAT_PORT not specified or set in %s" % args.config 
+    # assert args.ctat_port != None, "CTAT_PORT not specified or set in %s" % args.config
     assert args.al_dir != None, "AL_DIR not specified or set in %s" % args.config
-    # assert args.browser != None, "BROWSER not specified or set in %s" % args.config 
-    assert args.log_dir != None, "LOG_DIR not specified or set in %s" % args.config 
+    # assert args.browser != None, "BROWSER not specified or set in %s" % args.config
+    assert args.log_dir != None, "LOG_DIR not specified or set in %s" % args.config
 
     if(args.output == None):
         args.output = "%s/%sLog-%s.txt" % (args.log_dir , os.path.basename(args.training).split(".")[0], datetime.now().strftime("%Y-%m-%d-%H_%M_%S"))
 
     if(args.outer_loop_dir != None):
-        args.outer_loop_dir = os.path.abspath(apply_wd(args.outer_loop_dir))    
+        args.outer_loop_dir = os.path.abspath(apply_wd(args.outer_loop_dir))
 
 
-    
+
     args.output = os.path.abspath(apply_wd(args.output))
 
 
@@ -251,20 +252,24 @@ def parse_args(argv):
 def main(args):
     global al_process,ctat_process, browser_process, outer_loop_process
 
-    if(not args.al_port): args.al_port = get_open_port()
-    if(check_port(args.al_host, args.al_port, args.force)):
-        al_process =  subprocess.Popen([sys.executable, os.path.join(args.al_dir, "manage.py"), "runserver", str(args.al_host) + ":" + str(args.al_port)])
-        # al_thread = threading.Thread(target=waitAndExit, args=(al_process, kill_all))
-        # al_thread.start()
+    no_al = '--no-al' in sys.argv
+    if no_al:
+        assert args.al_port is not None, "Must specify AL_PORT in net.conf or command line"
     else:
-        port_error("AL", args.al_port)
+        if(not args.al_port): args.al_port = get_open_port()
+        if(check_port(args.al_host, args.al_port, args.force)):
+            al_process =  subprocess.Popen([sys.executable, os.path.join(args.al_dir, "manage.py"), "runserver", str(args.al_host) + ":" + str(args.al_port)])
+            # al_thread = threading.Thread(target=waitAndExit, args=(al_process, kill_all))
+            # al_thread.start()
+        else:
+            port_error("AL", args.al_port)
 
     if(not args.ctat_port): args.ctat_port = get_open_port()
     if(check_port(args.ctat_host, args.ctat_port, args.force)):
         ctat_process = subprocess.Popen([sys.executable, os.path.join("src", "host_server.py") , str(args.ctat_port), args.output])
         # ctat_thread = threading.Thread(target=waitAndExit, args=(ctat_process, kill_all))
         # ctat_thread.start()
-        
+
     else:
 
         port_error("CTAT", args.ctat_port)
@@ -272,7 +277,7 @@ def main(args):
 
     if(args.outer_loop_url is None and args.outer_loop):
         assert args.outer_loop_dir is not None, "Must specify OUTER_LOOP_DIR in net.conf"
-        if(not args.outer_loop_port): args.outer_loop_port = get_open_port()        
+        if(not args.outer_loop_port): args.outer_loop_port = get_open_port()
         if(check_port(args.outer_loop_host, args.outer_loop_port, args.force)):
             # print([sys.executable, os.path.join(args.outer_loop_dir, "src", "server.py") , "--host", str(args.outer_loop_host),"--port", int(args.outer_loop_port)])
             outer_loop_process = subprocess.Popen([sys.executable, os.path.join(args.outer_loop_dir, "server.py") , "--host", str(args.outer_loop_host),"--port", str(args.outer_loop_port)])
@@ -297,7 +302,7 @@ def main(args):
     if(args.nools_dir): ctat_url += "&nools_dir=%s" % args.nools_dir
     if(args.outer_loop_url): ctat_url += "&outer_loop_url=%s" % args.outer_loop_url
 
-    
+
     if(args.browser != None and "selenium" in args.browser):
         from selenium import webdriver
         sys.path.append("/home/danny/Projects")
@@ -313,10 +318,10 @@ def main(args):
         elif("firefox" in args.browser):
 
             #             geckodriver = 'C:\\Users\\grayson\\Downloads\\geckodriver.exe'
- 
 
- 
-            # browser = 
+
+
+            # browser =
             options = webdriver.FirefoxOptions()
             # options.add_argument('--ignore-certificate-errors')
             # options.add_argument("--test-type")
@@ -339,7 +344,7 @@ def main(args):
     # al_process.wait()
     # print("AL PROCESS")
     while True:
-        if(al_process.poll() != None or ctat_process.poll() != None):
+        if((not no_al and al_process.poll() != None) or ctat_process.poll() != None):
             break
         # try:
         time.sleep(.1)
