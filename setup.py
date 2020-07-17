2c83af99363242093a5593c0614cf0f825e49cbe
Erik Harpstead <eharpste@cs.cmu.edu>
2020-07-17 16:00:00 -0400

    Updated setup
    Updated the setup file and gitignore files to reflect the name change.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open('requirements.txt') as f:
 long_description = '...need to add description' 
   
 setup( 
-        name ='alhtml', 
+        name ='altrain', 
         version ='0.4.4', 
         author ='Daniel Weitekamp', 
         author_email ='weitekamp@cmu.edu', 
-        url ='https://github.com/apprenticelearner/AL_HTML', 
+        url ='https://github.com/apprenticelearner/AL_Train', 
         description ='Runs Apprentice Learner agents against in tutors.', 
         long_description = long_description, 
         long_description_content_type ="text/markdown", 
@@ -30,7 +30,7 @@ setup(
             "License :: OSI Approved :: MIT License", 
             "Operating System :: OS Independent", 
         ), 
-        keywords ='apprentice learner html bridge', 
+        keywords ='apprentice learner training harness', 
         install_requires = requirements, 
         zip_safe = False
 ) 
