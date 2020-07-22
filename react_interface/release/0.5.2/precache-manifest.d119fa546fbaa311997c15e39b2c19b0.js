80a57177aebe2afa4ca43c27c1681771eeed878d
Danny Weitekamp <dannyweitekamp@gmail.com>
2020-07-22 10:21:01 -0400

    added release 0.5.2

---
+++ react_interface/release/0.5.2/precache-manifest.d119fa546fbaa311997c15e39b2c19b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de36bf45e37aa0d55fbf",
+    "url": "./static/css/main.c622a072.chunk.css"
+  },
+  {
+    "revision": "de36bf45e37aa0d55fbf",
+    "url": "./static/js/main.de36bf45.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "cf2eecdbce5c7bf43aa9",
+    "url": "./static/js/2.cf2eecdb.chunk.js"
+  },
+  {
+    "revision": "3fbf838fc517c60c80378318b34d3be4",
+    "url": "./static/media/Arrowhead-01-128.3fbf838f.png"
+  },
+  {
+    "revision": "912d4e8d29898ad63f4eae9bf5bf7b6a",
+    "url": "./static/media/Arrowhead-Down-01-128.912d4e8d.png"
+  },
+  {
+    "revision": "33f0258525e2650ae5740dc733a1fd72",
+    "url": "./postMock.html"
+  },
+  {
+    "revision": "fbdb8390938418af10fe71703a02b37c",
+    "url": "./index.html"
+  }
+];
