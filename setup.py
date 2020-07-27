c7514eee5a00e21448e6581eeb872559452586f7
Danny Weitekamp <dannyweitekamp@gmail.com>
2020-07-27 15:59:41 -0400

    v0.5.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = '...need to add description'
   
 setup( 
         name ='al_train', 
-        version ='0.5.2', 
+        version ='0.5.6', 
         author ='Daniel Weitekamp', 
         author_email ='weitekamp@cmu.edu', 
         url ='https://github.com/apprenticelearner/AL_Train', 
