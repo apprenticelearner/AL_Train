901ccb180f47ffa5e5c4a40e6a92f2ab6fc5caf9
JohnPedraza <jpedraza1123@gmail.com>
2021-10-19 04:42:44 -0400

    Add result parser

---
+++ result_parser.py
@@ -0,0 +1,7 @@
+# Author: John Pedraza
+# October 19th, 2021
+
+# This file is used to parse the results of memory-related experiments 
+# for the Apprentice Learner architecture
+
+import matplotlib.pyplot as plt
